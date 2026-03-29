ba50cde8d45fa0056d44fd4092c557458719efc6
Towhedul <towhidshamim009@gmail.com>
2026-03-29 16:31:02 +0600

    vedio section and Features section

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import Hero from './Hero'
 import FeaturedCars from './FeaturedCars'
+import VideoSection  from './VedioSection'
+import Features from './Features'
 
 const Home = () => {
   return (
     <div>
         <Hero/>
         <FeaturedCars/>
+        <VideoSection/>
+        <Features/>
     </div>
   )
 }
